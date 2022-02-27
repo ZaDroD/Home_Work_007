312b2083a06ff5262fb013d06a69484b9200f73f
Evgeniy <zadrod93@yandex.ru>
2022-02-27 19:17:16 +0300

    Задача номер 50

---
+++ Example050/Program.cs
@@ -0,0 +1,33 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные.
+
+double[,] mass = new double[5, 6];
+double count = 10;
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = count;
+            count++;
+        }
+    }
+
+}
+void PrintMassiv(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0)
+            {
+                array[i, j] = array[i, j] * -1;
+            }
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(mass);
+PrintMassiv(mass);
