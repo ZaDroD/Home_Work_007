5d87a55cdb1ca7ed8eebea46d4d7fd584c21a45b
Evgeniy <zadrod93@yandex.ru>
2022-02-27 19:13:02 +0300

    Задача номер 48

---
+++ Example048/Program.cs
@@ -0,0 +1,27 @@
+﻿//48. Показать двумерный массив размером m×n заполненный целыми числами.
+int[,] mass = new int[5, 6];
+Random rand = new Random();
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(0, 10);
+        }
+    }
+
+}
+void PrintMassiv(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(mass);
+PrintMassiv(mass);
